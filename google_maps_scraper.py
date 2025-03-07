4fff2187c1501987efa768555f60a76589e0ba03
Jalil <benharkatdjalil@gmail.com>
2025-03-07 21:54:23 +0400

    bypass initials

--- google_maps_scraper.py
+++ google_maps_scraper.py
@@ -150,6 +150,12 @@ class GoogleMaps:
             return None
 
     def click_coordinates(self, x, y):
+        accept_cookies_css_selector = selectors["accept_cookies"]["css_selector"]
+        if self.wait_for_css_selector(accept_cookies_css_selector, timeout=10):
+            self.driver.find_element(
+                by=By.CSS_SELECTOR, value=accept_cookies_css_selector
+            ).click()
+        time.sleep(10)
         coordinates_css_selector = selectors["coordinates"]["css_selector"]
         actions = ActionChains(self.driver)
         body = self.driver.find_element(By.TAG_NAME, "body")
